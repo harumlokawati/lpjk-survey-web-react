60526c3a46794c1115198bda97d7cc5c71b5746f
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-07-26 15:31:38 +0700

    add redux integration

--- app/index.js
+++ app/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 import App from './pages/App.js'
 
+// Reducer
+import { reducer as surveyReducer } from 'reducers/survey/index'
+
+const reducer = {
+  survey: surveyReducer
+}
+
+const store = createStore(combineReducers(reducer))
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 )
