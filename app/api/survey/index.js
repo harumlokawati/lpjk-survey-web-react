7889e2e0e2dedbf449b784e65e074195b53aa8c0
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-07-26 18:13:24 +0700

    add simple api get survey data

---
+++ app/api/survey/index.js
@@ -0,0 +1,9 @@
+export function getSurveyData () {
+  return axios.get("http://localhost:3002/survey")
+    .then(res => {
+      return Promise.resolve(res)
+    })
+    .catch(err => {
+      return Promise.reject(err)
+    })
+}
