16376070f567d6c7262967720ec3fa7e5367ed0f
roselinapradjanata <roselinapradjanata@gmail.com>
2018-08-10 16:23:49 +0700

    Remove unused import

--- app/pages/Company/Profile/Profile.js
+++ app/pages/Company/Profile/Profile.js
@@ -1,10 +1,8 @@
-import SurveyForm from 'components/SurveyForm'
 import ProfileForm from 'components/ProfileForm'
 
 class Profile extends React.Component {
   render () {
     return (
-      //<SurveyForm onSubmit={this._onSubmitProfileForm} />
       <ProfileForm onSubmit={this._onSubmitProfileForm} />
     )
   }
