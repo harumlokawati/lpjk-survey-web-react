397a57757c8e1b4689d443d425082b580515993d
roselinapradjanata <roselinapradjanata@gmail.com>
2018-08-10 16:22:43 +0700

    Create company profile page

---
+++ app/components/ProfileForm/constants.js
@@ -0,0 +1,4 @@
+export const COMPANY_NAME = 'Nama'
+export const COMPANY_LOCATION = 'Lokasi'
+export const COMPANY_CATEGORY = 'Kategori'
+export const COMPANY_TYPE = 'Jenis'
--- app/pages/Company/Profile/Profile.js
+++ app/pages/Company/Profile/Profile.js
@@ -1,6 +1,17 @@
+import SurveyForm from 'components/SurveyForm'
+import ProfileForm from 'components/ProfileForm'
+
 class Profile extends React.Component {
   render () {
-    return (<div>Company Profile</div>)
+    return (
+      //<SurveyForm onSubmit={this._onSubmitProfileForm} />
+      <ProfileForm onSubmit={this._onSubmitProfileForm} />
+    )
+  }
+
+  _onSubmitProfileForm (values) {
+    // TODO: handle submit form
+    console.log(values)
   }
 }
 
