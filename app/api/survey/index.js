963187d5daaffd0795756d5f8c68eaa9c4719c2a
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-09-13 14:01:02 +0700

    Set Cookies global and lint

--- app/api/survey/index.js
+++ app/api/survey/index.js
@@ -22,7 +22,6 @@ export function submitSurveyData (request) {
     })
 }
 
-
 function generateRequestHeaders () {
   cookiesData = !cookiesData ? Cookies.get('access_token') : cookiesData
   return {
