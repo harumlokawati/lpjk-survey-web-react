c2fe1935572c43770c60811bf0aeac52c8de0a47
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-09-26 20:07:03 +0700

    Add dropdown component

---
+++ app/components/FormGroup/SelectFormGroup/SelectFormGroup.js
@@ -0,0 +1,33 @@
+import { Field } from 'redux-form'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+
+class SelectFormGroup extends React.Component {
+  renderSelectField = ({input, options}) => {
+    return (<FormControl>
+      <Select
+        variant='outlined'
+        autoWidth
+        {...input}
+      >
+        {options.map(option => {
+          return <MenuItem key={option.id} value={option.name}>{option.name}</MenuItem>
+        })}
+      </Select>
+    </FormControl>)
+  }
+
+  render () {
+    let {question, className, options} = this.props
+    return (
+      <div className={`form-group ${className}`}>
+        <p>{question.question}</p>
+        <hr />
+        <Field name={question.key} component={this.renderSelectField} options={options} inputName={question.key} />
+      </div>
+    )
+  }
+}
+
+export default (SelectFormGroup)
