017bec5cc40d912cce3cad394f3e68659a599b63
roselinapradjanata <roselina.911@gmail.com>
2018-10-19 12:24:36 +0700

    Make survey form in one column

--- app/components/SurveyForm/SurveyForm.js
+++ app/components/SurveyForm/SurveyForm.js
@@ -28,93 +28,113 @@ class SurveyForm extends React.Component {
         {data && <div className='survey-form'>
           <div className='container survey-form-header'>
             <img className='lpjk-logo img-fluid' src='http://lpjk.org/template/blue/images/logo_lpjkn_mainpage.jpg' />
-            <h3>Survey Penelitian Gambaran Teknologi Konstruksi Di Indonesia</h3>
+            <h4>Survey Penelitian Gambaran Teknologi Konstruksi Di Indonesia</h4>
           </div>
           <div className='container survey-form-content'>
             <hr />
             <div className='row mb-4'>
-              <CheckboxFormGroup className='col-md-6'
+              <CheckboxFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.CONSTRUCTION_PRODUCT_TYPE}
                 options={this.props.data.constructionProductType} />
-              <TextFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_APPLIED}
                 placeholder='Jawaban'
                 multiline />
             </div>
             <div className='row mb-4'>
-              <TextFormGroup className='col-md-6'
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.CONSTRUCTION_PROJECT}
                 placeholder='Jawaban'
                 multiline />
-              <TextFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.CONSTRUCTION_PROJECT_DATE}
                 placeholder='Jawaban'
                 multiline />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.CONSTRUCTION_PROJECT_COST}
                 options={this.props.data.constructionProjectCost} />
-              <CheckboxFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <CheckboxFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_STAGE}
                 options={this.props.data.technologyConstructionStage} />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_TYPE}
                 options={this.props.data.technologyConstructionType} />
-              <RadioButtonFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_ORIGIN}
                 options={this.props.data.technologyConstructionOrigin} />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_OWNER}
                 options={this.props.data.technologyConstructionOwner} />
-              <RadioButtonFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_CATEGORY}
                 options={this.props.data.technologyConstructionCategory} />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_PRIMARY_TYPE}
                 options={this.props.data.technologyConstructionPrimaryType} />
-              <CheckboxFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <CheckboxFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_COST}
                 options={this.props.data.technologyConstructionCost} />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_BENEFIT}
                 options={this.props.data.technologyConstructionBenefit} />
-              <TextFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_ALL_BENEFITS}
                 placeholder='Jawaban'
                 multiline />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_SUCCESS_FACTOR}
                 options={this.props.data.technologyConstructionSuccessFactor} />
-              <TextFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_ALL_SUCCESS_FACTORS}
                 placeholder='Jawaban'
                 multiline />
             </div>
             <div className='row mb-4'>
-              <RadioButtonFormGroup className='col-md-6'
+              <RadioButtonFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_OBSTACLE}
                 options={this.props.data.technologyConstructionObstacle} />
-              <TextFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <TextFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_ALL_OBSTACLES}
                 placeholder='Jawaban'
                 multiline />
             </div>
             <div className='row mb-4'>
-              <CheckboxFormGroup className='col-md-6'
+              <CheckboxFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_HUMAN_RESOURCE}
                 options={this.props.data.technologyConstructionHumanResource} />
-              <CheckboxFormGroup className='col-md-6'
+            </div>
+            <div className='row mb-4'>
+              <CheckboxFormGroup className='mx-auto col-md-6'
                 question={surveyQuestions.TECHNOLOGY_CONSTRUCTION_SUPPORT}
                 options={this.props.data.technologyConstructionSupport} />
             </div>
