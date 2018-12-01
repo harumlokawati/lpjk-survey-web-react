10da51271ac1456b5c44bc58efd5f0e26446b7d1
roselinapradjanata <roselina.911@gmail.com>
2018-12-01 21:56:55 +0700

    Use environment variables

--- app/api/constant.js
+++ app/api/constant.js
@@ -1,7 +1,6 @@
-// const BACKEND_PORT = process.env.BACKEND_PORT
-// const SERVICE_HOST = process.env.SERVICE_HOST
-// const HOST = `${SERVICE_HOST}:${BACKEND_PORT}`
-const HOST = 'http://localhost:4000'
+const SERVICE_PORT = process.env.SERVICE_PORT
+const SERVICE_HOST = process.env.SERVICE_HOST
+const HOST = `${SERVICE_HOST}:${SERVICE_PORT}`
 export const LOGIN_URL = `${HOST}/auth/login`
 export const REGISTER_URL = `${HOST}/auth/register`
 
